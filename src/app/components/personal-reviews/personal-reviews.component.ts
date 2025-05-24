d845833dd720855903d7ee646cfed96035f317b2
Fausto Fuertes <faustofuertes@Faustos-MacBook-Pro.local>
2025-05-24 18:04:10 -0300

    mejore reviews

--- src/app/components/personal-reviews/personal-reviews.component.ts
+++ src/app/components/personal-reviews/personal-reviews.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Review } from '../../interfaces/review';
 import { ReviewsService } from '../../services/reviews.service';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-personal-reviews',
@@ -12,13 +13,36 @@ import { CommonModule } from '@angular/common';
 export class PersonalReviewsComponent implements OnInit {
   reviews?: Review[];
   @Input() postId?: string;
+  isAdding = false;
+  needLogin = false;
 
-  constructor(private _myReviewService: ReviewsService) { }
+  constructor(
+    private _myReviewService: ReviewsService,
+    private _auth: AuthService
+  ) { }
 
   ngOnInit(): void {
     this._myReviewService.getReviewPerPost(this.postId).subscribe(data => {
       this.reviews = data;
-      console.log(this.reviews)
     })
+  }
+
+  toggleReview() {
+    this._auth.isAuthenticated$.subscribe(res => {
+      if (res) {
+        this.isAdding = !this.isAdding;
+      }
+      else {
+        this.needLogin = !this.needLogin;
+      }
+    })
+  }
+
+  login() {
+    this._auth.loginWithRedirect();
+  }
+
+  closeIfOutside(event: MouseEvent) {
+    this.isAdding = false;
   }
 }
