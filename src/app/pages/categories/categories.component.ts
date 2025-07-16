977905aee46e8a3ae31439356e4e0c3ba0b4d1a2
Fausto Fuertes <fausto.fuertes.83@gmail.com>
2025-07-16 16:29:36 -0300

    agregue pantalla de oficios no disponible

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, NavigationEnd, Router, RouterLink } from '@angular/rout
 import { CommonModule } from '@angular/common';
 import { PublicacionesService } from '../../services/publicaciones.service';
 import { Publicacion } from '../../interfaces/publicacion';
+import { Auth } from '@angular/fire/auth';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-categories',
@@ -21,43 +23,59 @@ export class CategoriesComponent implements OnInit {
   constructor(
     private _publiService: PublicacionesService,
     private _ar: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _auth: AuthService
   ) { }
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
     this.getJob(); // Carga inicial
-  
+
     this._router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.getJob();  // se ejecuta al terminar navegación
       }
     });
   }
-  
+
   getJob() {
     this.location = this._ar.snapshot.paramMap.get('ubicacion');
     this.job = this._ar.snapshot.paramMap.get('trabajo');
     this._publiService.getPublicacionesPorTrabajoYUbi(this.job, this.location)
-  .subscribe(data => {
-    this.list = this.shuffleArray([...data]); // copia y mezcla
-  });
+      .subscribe(data => {
+        this.list = this.shuffleArray([...data]); // copia y mezcla
+      });
 
   }
 
   switchJob(job: string) {
     if (this.job === job) return;
-  
+
     this.job = job;
     this._router.navigate(['/categorias/Mar del Plata', job]);
   }
-  
-   shuffleArray<T>(array: T[]): T[] {
+
+  shuffleArray<T>(array: T[]): T[] {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
   }
-  
+
+  register() {
+    this._auth.isAuthenticated$.subscribe((loggedIn) => {
+      if (loggedIn) {
+        this._router.navigate(['/crear-publicacion']);
+      } else {
+        this._auth.loginWithRedirect({
+          authorizationParams: {
+            redirect_uri: window.location.origin, // ← http://localhost:4200
+          },
+          appState: { target: '/crear-publicacion' } // ← correcta forma
+        });
+      }
+    });
+  }
+
 }
